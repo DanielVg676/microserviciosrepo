bf5318dc4bdf6b7c60e739995985bf8e8d1e157b
DANIEL <dv956543@gmail.com>
2025-03-12 21:43:50 -0600

    client-users enlazados creacion en cadena

--- clients/src/controllers/clientController.js
+++ clients/src/controllers/clientController.js
@@ -1,4 +1,11 @@
 import Client from "../models/clientModel.js";
+import { EventEmitter } from 'events';
+import axios from 'axios';
+
+
+// EVENT EMITTER PARA LA CREACION AUTOMATICA DE UN NUEVO USUARIO
+const eventEmitter = new EventEmitter();
+
 
 export const getClients = async (req, res) => {
     try {
@@ -24,6 +31,8 @@ export const getClientById = async (req, res) => {
     }
 };
 
+//CREAR UN CLIENTE CON LA INFO DE TODA LA TABLA
+
 export const createClient = async (req, res) => {
     try {
         const { name, lastName, email, phone, birthDate, address } = req.body;
@@ -36,12 +45,35 @@ export const createClient = async (req, res) => {
             address,
             status: true, // Nuevo cliente siempre inicia activo
         });
+
+        // Emitimos el evento cuando se crea un cliente
+        eventEmitter.emit('clientCreated', newClient);
+
         res.status(201).json(newClient);
     } catch (error) {
         console.error("Error al crear Cliente:", error);
         res.status(500).json({ message: "Error al crear el cliente" });
     }
 };
+
+// Escuchar el evento para crear un usuario automáticamente
+eventEmitter.on('clientCreated', async (client) => {
+    try {
+        // const randomUsername = `user${Math.floor(Math.random() * 10000)}`;
+        const randomPassword = Math.random().toString(36).slice(-8); // Genera una contraseña aleatoria de 8 caracteres
+
+        await axios.post('http://localhost:3003/api/users/newuser', {
+            username: client.email,
+            password: randomPassword,
+            phone: client.phone,
+        });
+
+        console.log(`Usuario creado automáticamente para el cliente ${client.name} ${client.lastName}`);
+    } catch (error) {
+        console.error('Error al crear usuario automáticamente:', error);
+    }
+});
+
 
 export const updateClient = async (req, res) => {
     try {
